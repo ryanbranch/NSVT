84903e916bc40f7e6db9e2425a0bebd231e5407e
ryanbranch <ryanbranch@users.noreply.github.com>
2020-07-30 10:17:59 -0400

    Animation Pause/Play, Image Scaling/Clearing/Randomization
    Enter (play) and Spacebar (pause) keys are now used to control the animation functionality
    
    Displayed images can now be a scaled (up or down) version of the computed Numpy array
    
    The "C" key now clears the image (setting it to a blank white canvas by default, optionally any other grayscale color value can be specified)
    
    Mouse clicking now randomizes the color of one pixel in the image

--- main.py
+++ main.py
@@ -24,34 +24,41 @@ DEFAULT_FRAME_NAME_DIGITS = 6
 DEFAULT_FRAME_NAME_SUFFIX = ""
 DEFAULT_FRAME_FORMAT = ".png"
 # IMAGE METADATA
-DEFAULT_IMAGE_WIDTH = 500
-DEFAULT_IMAGE_HEIGHT = 500
+DEFAULT_IMAGE_WIDTH = 100
+DEFAULT_IMAGE_HEIGHT = 100
 # NUMPY IMAGE (META)METADATA
-DEFAULT_NUMPY_DATATYPE = numpy.float32
+DEFAULT_NUMPY_DATATYPE = numpy.uint8
 # PIL METADATA
 DEFAULT_PIL_RESIZE_RESAMPLE_MODE = PIL.Image.NEAREST
 
 NUM_COLOR_CHANNELS = 3
-NUM_EXTRA_CHANNELS = 5
 
 APP_WIDTH = 1024
 APP_HEIGHT = 768
-FRAME_WIDTH = 500
-FRAME_HEIGHT = 500
 
-DEFAULT_FRAMERATE = 30
+DEFAULT_FRAMERATE = 20
+
+class PhysicsEngine():
+
+    def __init__(self, wrapper_):
+        self.wrapper = wrapper_
 
 class GraphicsEngine():
 
-    def __init__(self):
+    def __init__(self, wrapper_):
+
+        self.wrapper = wrapper_
 
         self.saveCount = 0
+        self.pilImageScaleX = 10.0
+        self.pilImageScaleY = 10.0
 
-        self.numpyImage = numpy.ones(shape=(DEFAULT_IMAGE_HEIGHT,
-                                           DEFAULT_IMAGE_WIDTH,
-                                           (NUM_COLOR_CHANNELS + NUM_EXTRA_CHANNELS)),
-                                     dtype=DEFAULT_NUMPY_DATATYPE) * 32
-        self.pilImage = Image.fromarray(self.numpyImage[:, :, 0:NUM_COLOR_CHANNELS].astype("uint8"))
+        self.numpyImage = numpy.ones(shape=(DEFAULT_IMAGE_HEIGHT, DEFAULT_IMAGE_WIDTH, NUM_COLOR_CHANNELS),
+                                     dtype=DEFAULT_NUMPY_DATATYPE
+                                     ) * 255
+        self.pilImage: Image
+        # Sets the value of self.pilImage
+        self.refreshPilImage()
 
         self.makeDirectories()
 
@@ -73,7 +80,14 @@ class GraphicsEngine():
     # NOTE: Call this function before any use of self.pilImage,
     #       unless it is clear that self.numpyImage has not been modified since the last refreshPilImage() call.
     def refreshPilImage(self):
-        self.pilImage = Image.fromarray(self.numpyImage[:, :, 0:NUM_COLOR_CHANNELS].astype("uint8"))
+        if (self.pilImageScaleX == 1.0 and self.pilImageScaleY == 1.0):
+            self.pilImage = Image.fromarray(self.numpyImage[:, :, 0:NUM_COLOR_CHANNELS].astype("uint8"))
+        else:
+            self.pilImage = Image.fromarray(self.numpyImage[:, :, 0:NUM_COLOR_CHANNELS].astype("uint8")).resize(
+                (int(self.numpyImage.shape[1] * self.pilImageScaleX),
+                 int(self.numpyImage.shape[0] * self.pilImageScaleY)),
+                DEFAULT_PIL_RESIZE_RESAMPLE_MODE)
+
 
     def getSavePath(self, fileIndex, digits=DEFAULT_SAVE_NAME_DIGITS, suffix=DEFAULT_SAVE_NAME_SUFFIX,
                     format=DEFAULT_SAVE_FORMAT):
@@ -86,8 +100,17 @@ class GraphicsEngine():
     def saveImage(self, path):
         Image.fromarray(self.numpyImage[:,:,0:NUM_COLOR_CHANNELS].astype("uint8")).save(path)
 
-    def manipulateImage(self):
-        print("Placeholder")
+    def randomizeRandomPixel(self):
+        self.numpyImage[random.randrange(self.numpyImage.shape[0]),
+                        random.randrange(self.numpyImage.shape[0])] = [random.randint(0, 255),
+                                                                       random.randint(0, 255),
+                                                                       random.randint(0, 255)]
+
+    @staticmethod
+    @jit(nopython=True)
+    def clearImageNumba(imageData, color=255):
+        imageData[:, :, :] = color
+        return imageData
 
     @staticmethod
     @jit(nopython=True)
@@ -95,7 +118,7 @@ class GraphicsEngine():
         dims = imageData.shape
         for y in range(dims[0]):
             for x in range(dims[1]):
-                imageData[y, x] += (y + x)
+                imageData[y, x] += numpy.uint8(y + x)
         #imageData += 32
         return imageData
 
@@ -108,64 +131,133 @@ class AppInfo():
 
 class App(tkinter.Tk):
 
-    def __init__(self, wrapper_, info_):
+    def __init__(self, wrapper_):
         # BASE CLASS CONSTRUCTOR
         tkinter.Tk.__init__(self)
 
         # MEMBER VARIABLE DEFINITIONS
         # References to external objects
         self.wrapper = wrapper_
-        self.info = info_
         # App Parameters
         self.msPerFrame = int(round(1000 / DEFAULT_FRAMERATE))
+        self.animating = False
+        self.frameTime = default_timer()
+        self.timeDelta = default_timer() - self.frameTime
+        self.currentFPS = DEFAULT_FRAMERATE
+        self.nextFrameComputed = False
+
+
 
         # WINDOW PROPERTY DEFINITIONS
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        # USER INPUT EVENT CONTROL
+        self.bind("<Button-1>", self.handleMouseClick)
+        self.bind("<Return>", self.handleKeyEnter)
+        self.bind("<space>", self.handleKeySpace)
+        self.bind("c", self.handleKeyC)
+        self.bind("d", self.handleKeyD)
+
 
         self.defineGraphics()
+        self.advance()
 
+    # Mouse Left Click - Advances the state
+    def handleMouseClick(self, event):
+        print(event.x, event.y)
+        self.wrapper.gEngines[-1].randomizeRandomPixel()
+        self.advance()
 
+    # Keyboard "C" Press - Clears/Resets the image
+    def handleKeyC(self, event):
+        if self.animating:
+            self.stopAnimating()
+        self.wrapper.gEngines[-1].numpyImage = self.wrapper.gEngines[-1].clearImageNumba(self.wrapper.gEngines[-1].numpyImage)
+        self.advance()
 
-        self.bind('<Button-1>', self.startAnimating)
+    # Keyboard "D" Press - Advances the state
+    def handleKeyD(self, event):
+        if self.animating:
+            self.stopAnimating()
+        self.advance()
 
-    def startAnimating(self, event):
-        self.oldTime = default_timer()
+    # Keyboard "ENTER" Press - Begins the animation process
+    def handleKeyEnter(self, event):
+        if not self.animating:
+            self.startAnimating()
+
+    # Keyboard "SPACE" Press - Pauses the animation process
+    def handleKeySpace(self, event):
+        self.stopAnimating()
+
+    def advance(self):
+        # Updates the graphics
+        self.drawGraphics()
+        # Computes the upcoming frame
+        self.computeNextFrame()
+
+    def stopAnimating(self):
+        self.animating = False
+
+    def startAnimating(self):
+        self.animating = True
+        self.frameTime = default_timer()
         self.animate()
 
     def animate(self):
-        # NOTE: self.after() call of self.animate() should be the FIRST ACTION in this method
-        self.after(self.msPerFrame, self.animate)
-        # These two lines print the amount of time that has elapsed since the last self.after() call
-        print(default_timer() - self.oldTime)
-        self.oldTime = default_timer()
+        while not self.nextFrameComputed:
+            self.after(10)
+        if self.animating:
+            # NOTE: self.after() call of self.animate() should be the FIRST ACTION in this method
+            self.after(self.msPerFrame, self.animate)
+            # These lines track the amount of time that has elapsed since the last self.after() call
+            self.timeDelta = default_timer() - self.frameTime
+            self.frameTime = default_timer()
+            self.currentFPS = int(round(1 / self.timeDelta))
 
-        # Updates the graphics
-        self.drawGraphics()
+            # Updates the graphics
+            self.drawGraphics()
+            # Computes the upcoming frame
+            self.computeNextFrame()
 
 
 
     def defineGraphics(self):
+        # Refresh the values of the PIL Image
         self.wrapper.gEngines[-1].refreshPilImage()
+        # Define self.graphicsTkImage based on the refreshed PIL Image
         self.graphicsTkImage = ImageTk.PhotoImage(self.wrapper.gEngines[-1].pilImage)
-        self.graphicsTkFrame = tkinter.Frame(self, width=self.info.width, height=self.info.height, bg="red")
+        # Define a tkinter Frame in which to place GUI elements
+        # WARNING: No width and height are specified for this frame, be careful!
+        self.graphicsTkFrame = tkinter.Frame(self, bg="red")
+        # Create a tkinter Label within the frame for displaying the image
         self.graphicsTkImageLabel = tkinter.Label(self.graphicsTkFrame, image=self.graphicsTkImage)
+        # Assign the Frame a grid location within the GUI window
         self.graphicsTkFrame.grid(row=0, column=0, sticky="nsew", ipadx=0, ipady=0)
+
+        self.fpsTkTextLabel = tkinter.Label(self.graphicsTkFrame, text=("FPS: NULL"))
+        self.fpsTkTextLabel.grid(row=0, column=0, sticky="nw", ipadx=0, ipady=0)
 
 
     def drawGraphics(self):
+        # REFRESH AND DISPLAY:
         # Refresh the values of the PIL Image
         self.wrapper.gEngines[-1].refreshPilImage()
         # Update self.graphicsTkImage to match the new PIL image
         self.graphicsTkImage = ImageTk.PhotoImage(self.wrapper.gEngines[-1].pilImage)
         # Update the image label to refer to the new self.graphicsTkImage value
         self.graphicsTkImageLabel = tkinter.Label(self.graphicsTkFrame, image=self.graphicsTkImage)
-        # Display the image label to the grid
+        # Assign the image label a grid location within its frame
         self.graphicsTkImageLabel.grid(row=0, column=0, sticky="nsew", ipadx=0, ipady=0)
 
+        self.fpsTkTextLabel = tkinter.Label(self.graphicsTkFrame, text=("FPS: " + str(self.currentFPS)))
+        self.fpsTkTextLabel.grid(row=0, column=0, sticky="nw", ipadx=0, ipady=0)
+
+    def computeNextFrame(self):
         # Compute a new frame to prepare for future display
         self.wrapper.gEngines[-1].numpyImage = self.wrapper.gEngines[-1].manipulateImageNumba(self.wrapper.gEngines[-1].numpyImage)
+        self.nextFrameComputed = True
 
 class Wrapper():
 
@@ -174,16 +266,19 @@ class Wrapper():
         # MEMBER VARIABLE DECLARATION
         # AppInfo instance
         self.appInfo = AppInfo(APP_WIDTH, APP_HEIGHT)
-        # Frame instances
+        # GraphicsEngine instances
         self.gEngines = []
-        self.gEngines.append(GraphicsEngine())
+        self.gEngines.append(GraphicsEngine(self))
+        # PhysicsEngine instances
+        self.pEngines = []
+        self.pEngines.append(PhysicsEngine(self))
 
         # SETUP ACTIONS
         # Placeholder
 
         # POST-SETUP ACTIONS
         # Create and run the Tkinter GUI instance
-        self.app = App(self, self.appInfo)
+        self.app = App(self)
         self.app.mainloop()
 
 def main():
